5d1a6414a20a3329568e65bbffc6fe347f8dd349
Lily Lake <robinlake@macbookpro.lan>
2025-03-09 19:59:01 -0700

    remove comments

--- volumegate.ino
+++ volumegate.ino
@@ -5,29 +5,13 @@
 #include <SerialFlash.h>
 
 AudioInputI2S2           i2s2_1;         //xy=116,264
-// AudioEffectGranular      granular1;      //xy=298,326
 AudioOutputI2S           i2s1;           //xy=442,259
-// AudioMixer4              mixer1;         //xy=279,269
 AudioAmplifier           amp1;           //xy=253,291
 AudioAmplifier           amp2;           //xy=253,291
-
-
-// AudioConnection          patchCord1(i2s2_1, 0, mixer1, 0);
-// AudioConnection          patchCord2(i2s2_1, 1, mixer1, 1);
-// AudioConnection          patchCord3(mixer1, 0, i2s1, 1);
-// AudioConnection          patchCord4(mixer1, 0, i2s1, 0);
-// AudioConnection          patchCord6(granular1, 0, mixer1, 1);
-// AudioConnection          patchCord7(i2s2_1, 1, granular1, 0);
 AudioConnection          patchCord1(i2s2_1, 0, amp1, 0);
 AudioConnection          patchCord2(amp1, 0, i2s1, 0);
 AudioConnection          patchCord3(i2s2_1, 1, amp1, 0);
 AudioConnection          patchCord4(amp1, 0, i2s1, 1);
-// AudioConnection          patchCord7(amp1, 0, mixer1, 0);
-// AudioConnection          patchCord6(granular1, 0, i2s1, 1);
-// AudioConnection          patchCord7(i2s2_1, 1, granular1, 0);
-
-// #define GRANULAR_MEMORY_SIZE 12800  // enough for 290 ms at 44.1 kHz
-// int16_t granularMemory[GRANULAR_MEMORY_SIZE];
 
 uint32_t  mytime = 0;
 float amplitude = 1.0;
@@ -37,11 +21,6 @@ int release = 1000;
 
 void setup() {
   AudioMemory(20);
-    // granular1.begin(granularMemory, GRANULAR_MEMORY_SIZE);
-
-      // sine1.frequency(440);
-      // mixer1.gain(3, 1.0);
-
 }
 
 
@@ -50,7 +29,8 @@ void release_rise() {
   float step = (amplitude - volume_floor) / release;
   for (int i = 0; i < release; i++) {
     current_gain += step;
-    // mixer1.gain(1, current_gain);
+    // amp1.gain( current_gain);
+    // amp2.gain( current_gain);
     amp1.gain(current_gain);
     amp2.gain(current_gain);
     delay(1);
@@ -58,15 +38,18 @@ void release_rise() {
 }
 
 void duck() {
-//  mixer1.gain(1, volume_floor);
+//  amp1.gain( volume_floor);
+//  amp2.gain( volume_floor);
  delay(hold);
  release_rise();
-//  mixer1.gain(1, amplitude);
+//  amp1.gain(1, amplitude);
+//  amp2.gain(1, amplitude);
 }
 
 
 void loop() {
-  // mixer1.gain(1,amplitude);
+  // amp1.gain(amplitude);
+  // amp2.gain(amplitude);
   //   delay(4000);
   duck();
   delay(4000);
