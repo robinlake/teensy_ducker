91e60ce3f4a2f30ba1d5d1450eaab9ff40c1df32
Lily Lake <robinlake@Lilypad.local>
2025-03-13 20:05:16 -0700

    gonna replace with two input code

--- volumegate.ino
+++ volumegate.ino
@@ -16,11 +16,14 @@ AudioConnection          patchCord4(amp1, 0, i2s1, 1);
 uint32_t  mytime = 0;
 float amplitude = 1.0;
 float volume_floor = 0.0;
-int hold = 140;
-int release = 1000;
+int hold = 500;
+int release = 100;
+int a1history=0;
 
 void setup() {
   AudioMemory(20);
+    Serial.begin(115200);
+    a1history = analogRead(A1);
 }
 
 
@@ -29,8 +32,6 @@ void release_rise() {
   float step = (amplitude - volume_floor) / release;
   for (int i = 0; i < release; i++) {
     current_gain += step;
-    // amp1.gain( current_gain);
-    // amp2.gain( current_gain);
     amp1.gain(current_gain);
     amp2.gain(current_gain);
     delay(1);
@@ -38,12 +39,24 @@ void release_rise() {
 }
 
 void duck() {
-//  amp1.gain( volume_floor);
-//  amp2.gain( volume_floor);
+ amp1.gain(volume_floor);
+ amp2.gain(volume_floor);
  delay(hold);
  release_rise();
-//  amp1.gain(1, amplitude);
-//  amp2.gain(1, amplitude);
+}
+
+// code for turning knob
+void wait(unsigned int milliseconds) {
+  elapsedMillis msec=0;
+    while (msec <= milliseconds) {
+      int a1 = analogRead(A1);
+      if (a1 > a1history + 50 || a1 < a1history - 50) {
+        Serial.print("Knob (pin A1) = ");
+        Serial.println(a1);
+        hold = a1;
+        a1history = a1;
+      }
+    }
 }
 
 
@@ -51,9 +64,8 @@ void loop() {
   // amp1.gain(amplitude);
   // amp2.gain(amplitude);
   //   delay(4000);
-  duck();
-  delay(4000);
 
-
-
+  // duck();
+  //   wait(250);
+  // delay(2000);
 }
