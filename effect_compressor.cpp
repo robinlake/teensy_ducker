97043541362c4eb8ad45ba9ab664812d693e68bb
Lily Lake <robinlake@Lilypad.local>
2025-03-25 10:42:10 -0700

    move squaring to dbfs, add value hack, still garbling sound

--- effect_compressor.cpp
+++ effect_compressor.cpp
@@ -66,14 +66,24 @@ bool AudioEffectCompressor::set_default_values(float compression_threshold,
 }
 
 int max_sample = 32767;
+// still giving some eroneous values
+// they are very negative numbers
+// temporary fix: set to minimum value for these numbers
+float min_dBFS = -120.0f;
+float sample_to_dBFS(int sample) {
 
-float dBFS(int sample) {
+  // calculate the instantaneous signal power (square the signal)
+  sample = pow(sample, 2);
+  sample = sqrt(sample);
   float output = sample;
   if (output == max_sample) {
     output = 0.0f;
     return output;
   }
   output = 20 * log10f(output / max_sample);
+  if (output < min_dBFS) {
+    return min_dBFS;
+  }
   return output;
 }
 
@@ -85,14 +95,19 @@ short dBFS_to_sample(float dBFS) {
   // todo: apply the opposite of the dBFS function
   // convert back into a short that cen be used for output
   float log_of_max = log10f(max_sample);
-  Serial.print("log of max = ");
-  Serial.println(log_of_max);
-  float left_side = log_of_max + (dBFS / 20);
-  Serial.print("left side = ");
-  Serial.println(left_side);
+  // Serial.print("log of max = ");
+  // Serial.println(log_of_max);
+  float left_side;
+  if (dBFS == 0.0f) {
+    left_side = log_of_max;
+  } else {
+    left_side = log_of_max + (dBFS / 20);
+  }
+  // Serial.print("left side = ");
+  // Serial.println(left_side);
   float answer = pow(left_side, 10);
-  Serial.print("answer = ");
-  Serial.println(answer);
+  // Serial.print("answer = ");
+  // Serial.println(answer);
   short output = answer;
   return output;
 }
@@ -109,20 +124,17 @@ float calculate_volume_db(audio_block_t *block) {
   float total = 0.0f;
   for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
     int datum = data[i];
-    // calculate the instantaneous signal power (square the signal)
-    datum = pow(datum, 2);
-    datum = sqrt(datum);
-    datum = dBFS(datum);
+    datum = sample_to_dBFS(datum);
     total += datum;
   }
   float sample_count = AUDIO_BLOCK_SAMPLES;
   float average = total / sample_count;
-  // if (count % 100 == 0) {
-  //   Serial.print("total = ");
-  //   Serial.println(total);
-  //   Serial.print("average = ");
-  //   Serial.println(average);
-  // }
+  if (count % 100 == 0) {
+    Serial.print("total = ");
+    Serial.println(total);
+    Serial.print("average = ");
+    Serial.println(average);
+  }
 
   return average;
 }
@@ -133,7 +145,7 @@ void compress_block(audio_block_t *block) {
 
   for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
     int sample = data[i];
-    float sample_dbfs = dBFS(sample);
+    float sample_dbfs = sample_to_dBFS(sample);
     // todo: apply compression ratio to sample_dbfs
     int compressed_sample = dBFS_to_sample(sample_dbfs);
     data[i] = compressed_sample;
@@ -141,13 +153,13 @@ void compress_block(audio_block_t *block) {
 }
 void AudioEffectCompressor::update(void) {
   audio_block_t *block;
-  short *bp;
+  // short *bp;
   block = receiveWritable(0);
 
   if (!block)
     return;
 
-  bp = block->data;
+  // bp = block->data;
 
   // audio_block_t *compressed_block = AudioEffectCompressor::allocate(20);
   audio_block_t *compressed_block;
@@ -163,13 +175,29 @@ void AudioEffectCompressor::update(void) {
   if (volume_db > compression_threshold) {
     compress_block(compressed_block);
     // Serial.println("trigger compression");
-    transmit(compressed_block, 0);
-    release(compressed_block);
-    release(block);
-  } else {
-    // transmit the block and release memory
-    transmit(block, 0);
-    release(block);
-    release(compressed_block);
+
+    if (count % 100 == 0) {
+      Serial.print("block samples: ");
+      for (int i = AUDIO_BLOCK_SAMPLES - 1; i >= 0; i--)
+        Serial.print(block->data[i]);
+      Serial.print(", ");
+      Serial.println("");
+    }
+
+    Serial.print("block samples: ");
+    for (int i = AUDIO_BLOCK_SAMPLES - 1; i >= 0; i--)
+      Serial.print(compressed_block->data[i]);
+    Serial.print(", ");
+    Serial.println("");
   }
+  transmit(compressed_block, 0);
+  release(compressed_block);
+  release(block);
+}
+else {
+  // transmit the block and release memory
+  transmit(block, 0);
+  release(block);
+  release(compressed_block);
+}
 }
